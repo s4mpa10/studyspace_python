8eb036103003550e64bbdcca9e5650bca372c6e9
s4mpa10 <universitario.alan777@gmail.com>
2025-06-21 12:25:26 -0300

    Adicioando os scripts da aula 04

--- Aula04/desafio01.py
+++ Aula04/desafio01.py
@@ -1,11 +1,11 @@
 # ---------------------- Desafio 01 propostos na aula 04: ----------------------
 # Questã: Crie um script Python que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado.
 
-# # Como a questão e o nível da aula está pedindo:
-# nome = input('Qual o seu nome? ')
+# Como a questão e o nível da aula está pedindo:
+nome = input('Qual o seu nome? ')
 
-# # Exibição da informação através da função 'print()'
-# print('Olá', nome, '! Prazer em te conhecer!')
+# Exibição da informação através da função 'print()'
+print('Olá', nome, '! Prazer em te conhecer!')
 
 
 # Outra forma para ser realizada:
